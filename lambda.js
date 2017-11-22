0fcc5761137d848d15c80b9ded2ff07cc60467e8
hashibiroko <m411momo@icloud.com>
2017-11-22 17:03:42 +0900

    implements getting github contributes

--- lambda.js
+++ lambda.js
@@ -1,21 +1,44 @@
-var twitter = require('twitter');
+const twitter = require('twitter');
+const Xray = require('x-ray');
 
-var client = new twitter({
+const x = Xray();
+const userName = process.env.GITHUB_USERNAME;
+const url = `https://github.com/users/${userName}/contributions`;
+
+const client = new twitter({
     consumer_key: process.env.CONSUMER_KEY,
     consumer_secret: process.env.CONSUMER_SECRET,
     access_token_key: process.env.ACCESS_TOKEN_KEY,
     access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 });
 
+const tweet = (text, func) => client
+    .post('statuses/update', {status: text}, (error, tweet) => func(error, tweet));
+
 exports.handler = (event, context, callback) => {
-    // callback(null, 'Hello from Lambda');
-    client.post('statuses/update', {
-        status: 'Hello from Lambda'
-    }, (error, tweet, response) => {
-        if (!error) {
-            callback(null, tweet);
-        } else {
-            callback(null, error);
-        }
+    x(url, 'rect', [{
+        count: '@data-count',
+        data: '@data-date',
+    }])((err, res) => {
+        let yesterday = res[res.length - 2]["count"];
+        let counts = res.map(value => Number(value["count"]));
+        let lastYear = counts.reduce((prev, value) => prev + value);
+
+        console.log("public contributes");
+        console.log(`yesterday: ${yesterday["count"]}`);
+        console.log(`last year: ${lastYear}`);
+
+        let text = `【コントリビュート定期 : ${userName}】`;
+        text += `\n　昨日は、${yesterday} 回だったよ`;
+        text += `\n　年間合計は、${lastYear} 回になったよ`;
+        text += `\n　https://github.com/${userName}`;
+
+        tweet(text, (error, tweet) => {
+            if (error) {
+                callback(null, error);
+            } else {
+                callback(null, tweet);
+            }
+        });
     });
 };
